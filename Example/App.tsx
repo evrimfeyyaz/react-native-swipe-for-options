fb1a1582c8b7c595e75b7582a9d7b5902a6b0619
Evrim Persembe <evrimfeyyaz@gmail.com>
2021-03-02 13:36:01 +0300

    Set up tsconfig.json and package.json, show a simple red box in the example app

--- Example/App.tsx
+++ Example/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Test } from "react-native-swipe-for-options";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Test />
     </View>
   );
 }
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from "react";
+import { View } from "react-native";
+
+export const Test: FC = () => {
+  return <View style={{ height: 100, width: 100, backgroundColor: "red" }} />
+}
